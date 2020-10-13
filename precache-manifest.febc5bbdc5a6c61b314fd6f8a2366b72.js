c14e19d14a1087466a0a28aaa35aadcf63ae3eaf
alexcostigan <alexcostigan@icloud.com>
2020-10-13 11:17:40 +0100

    Updates

---
+++ precache-manifest.febc5bbdc5a6c61b314fd6f8a2366b72.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c2aa5673ee5cf907b770b4385bb033f",
+    "url": "/codepen-clone/index.html"
+  },
+  {
+    "revision": "293007eba5c70d141ab9",
+    "url": "/codepen-clone/static/css/2.ce280647.chunk.css"
+  },
+  {
+    "revision": "b7f1b74e607932a15914",
+    "url": "/codepen-clone/static/css/main.8d3f83c9.chunk.css"
+  },
+  {
+    "revision": "293007eba5c70d141ab9",
+    "url": "/codepen-clone/static/js/2.df611ca6.chunk.js"
+  },
+  {
+    "revision": "0d6c4affe2a659999d44765f9a17cb4f",
+    "url": "/codepen-clone/static/js/2.df611ca6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7f1b74e607932a15914",
+    "url": "/codepen-clone/static/js/main.c2814d1b.chunk.js"
+  },
+  {
+    "revision": "7e95b33d3c3f8b32c9c4",
+    "url": "/codepen-clone/static/js/runtime-main.3a6f25c4.js"
+  }
+]);
